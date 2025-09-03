3c731385e356ca2d59ba7f0cd3911452256fd711
suvajit <suvajit.me@gmail.com>
2025-09-03 22:31:08 +0530

    fix: clan war league endpoint

--- libs/clash-client/src/clash-client.service.ts
+++ libs/clash-client/src/clash-client.service.ts
@@ -1,4 +1,5 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
+import { APIClanWar, APIClanWarLeagueGroup } from 'clashofclans.js';
 import { ClashClient } from './client';
 
 @Injectable()
@@ -33,10 +34,36 @@ export class ClashClientService {
     return body;
   }
 
-  async getClanWarLeagues(clanTag: string) {
+  async getClanWarLeague(clanTag: string) {
     const { body, res } = await this.clashClient.getClanWarLeagueGroup(clanTag);
-    if (!res.ok) throw new NotFoundException(`Clan ${clanTag} not found.`);
+    if (!res.ok) throw new NotFoundException(`Clan ${clanTag} is not in CWL.`);
+    const rounds = body.rounds.filter((round) => !round.warTags.includes('#0'));
 
-    return body;
+    const warTags = rounds.map((round) => round.warTags).flat();
+    const result = await Promise.all(
+      warTags.map((warTag) => this.getLeagueRoundWithWarTag(warTag)),
+    );
+
+    const payload: {
+      leagueGroup: APIClanWarLeagueGroup;
+      rounds: (APIClanWar & { round: number; warTag: string })[];
+    } = {
+      leagueGroup: body,
+      rounds: [],
+    };
+
+    for (const { body, warTag } of result) {
+      if (!body) continue;
+      const round = rounds.findIndex(({ warTags }) => warTags.includes(warTag)) + 1;
+      payload.rounds.push({ ...body, round, warTag });
+    }
+
+    return payload;
+  }
+
+  private async getLeagueRoundWithWarTag(warTag: string) {
+    const { res, body } = await this.clashClient.getClanWarLeagueRound(warTag);
+    if (!res.ok) return { body: null, warTag };
+    return { body, warTag };
   }
 }
