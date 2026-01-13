da1ab60fbb108bc8210716b3c19a6ba43fcdb28e
suvajit <suvajit.me@gmail.com>
2026-01-13 14:42:34 +0530

    fix: queue stack

--- src/exports/services/exports-consumer.service.ts
+++ src/exports/services/exports-consumer.service.ts
@@ -1,18 +1,22 @@
 import { JobTypes, QueueTypes } from '@app/constants';
 import { Process, Processor } from '@nestjs/bull';
+import { Logger } from '@nestjs/common';
 import { Job } from 'bull';
 import { ExportMembersInput } from '../dto';
 import { ExportsMembersService } from '../exports-members.service';
 
 @Processor(QueueTypes.EXPORT)
 export class ExportsConsumerService {
+  private logger = new Logger(ExportsConsumerService.name);
   constructor(private exportsMembersService: ExportsMembersService) {}
 
   @Process({ name: JobTypes.EXPORT_MEMBERS, concurrency: 100 })
   async process(job: Job<ExportMembersInput>) {
-    const result = await this.exportsMembersService.exportClanMembers(job.data);
-    console.log(result);
-
-    return job.progress(100);
+    try {
+      await this.exportsMembersService.exportClanMembers(job.data);
+    } catch (error) {
+      this.logger.error(error.message);
+      await job.moveToFailed({ message: error.message });
+    }
   }
 }
