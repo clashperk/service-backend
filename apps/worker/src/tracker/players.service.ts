893ee65d0b9e6626de46355044bf260f2d790c96
suvajit <suvajit.me@gmail.com>
2026-08-02 10:34:46 +0530

    debug: players tracking

--- apps/worker/src/tracker/players.service.ts
+++ apps/worker/src/tracker/players.service.ts
@@ -227,6 +227,7 @@ export class PlayersService {
           action: 'UNKNOWN',
         });
 
+        console.log(`${payload.tag} seasons.${payload.seasonId} [wars]`);
         await this.db.collection(Collections.PLAYERS).updateOne(
           { tag: payload.tag },
           {
